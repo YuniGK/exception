f8dab23527814df33431b51419bd99ed43f26853
yunkim <1106qwe@hanmail.net>
2022-09-01 18:24:51 +0900

    오류 처리 방법 주석-

--- src/main/java/com/example/exception/advice/GlobalControllerAdvice.java
+++ src/main/java/com/example/exception/advice/GlobalControllerAdvice.java
@@ -12,10 +12,15 @@ import javax.el.MethodNotFoundException;
 
 /*
 전체적인 오류를 모두 잡는다.
+
+@ControllerAdvice - 예외처리 시 사용한다.
+@RestControllerAdvice - @ControllerAdvice와 @ResponseBody를 합쳐 내용이다.
+단순히 예외만 처리하고 싶다면 @ControllerAdvice를 적용하면 되고, 응답으로 객체를 리턴해야 한다면 @RestControllerAdvice를 적용
 */
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
+    //ExceptionHandler - Controller계층에서 발생하는 에러를 잡아서 메서드로 처리해주는 기능, Service, Repository에서 발생하는 에러는 제외
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity exception(Exception e){
         System.out.println("name = " + e.getClass().getName());
--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -1,6 +1,9 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,6 +27,12 @@ public class ApiController {
     public User post(@Valid @RequestBody User user){
         System.out.println("user = " + user);
         return user;
+    }
+
+    //GlobalExceptionHandler과 다르게 해당 코드가 있는 부분의 오류만 확인이 가능하. 우선순위가 GlobalExceptionHandler보다 높다
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 }
